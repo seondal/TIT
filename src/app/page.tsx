6faa1accfc5bc9a258c0a0992e0a6b7e94315989
seondal <sseondal@gmail.com>
2023-09-01 00:15:52 +0900

    ✨ feat :  입력받아서 리스트로 저장

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,32 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
 
 export default function Home() {
+  const [input, setInput] = useState("");
+  const [idList, setIdList] = useState<string[]>([]);
+
+  function onSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    setIdList((prev) => [input, ...prev]);
+    setInput("");
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex flex-col items-center justify-between p-24">
+      <h1>Github Profile Table MD Generator</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          id="id"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+      </form>
+      <ul>
+        {idList.map((id, idx) => (
+          <li key={idx}>{id}</li>
+        ))}
+      </ul>
+    </main>
   );
 }
