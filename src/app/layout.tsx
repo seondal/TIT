9061ee51a0e318469006922561b31304ee1a842c
seondal <sseondal@gmail.com>
2023-08-31 23:56:44 +0900

    🎉 init : Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Profile Table MD",
+  description: "깃허브 리드미에 사용 할 수 있는 프로필 테이블 자동 생성기",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ko">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
